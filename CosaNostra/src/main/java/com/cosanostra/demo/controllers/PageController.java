2a62bbf2048f426bb613751e8755eca9d6e8169e
Victor Fritz <victorfritz.ici@gmail.com>
2019-03-18 11:23:52 +0100

    add controller

--- CosaNostra/src/main/java/com/cosanostra/demo/controllers/PageController.java
+++ CosaNostra/src/main/java/com/cosanostra/demo/controllers/PageController.java
@@ -1,10 +1,13 @@
 package com.cosanostra.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class PageController {
 	
+	@GetMapping("/")
 	public String index() {
 		return "marc la pute";
 	}
