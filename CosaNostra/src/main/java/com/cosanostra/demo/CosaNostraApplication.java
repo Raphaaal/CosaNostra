2fbb1e3dd1595c34b6f6ece4d3f8512fe0201b60
Vicftz <36641184+Vicftz@users.noreply.github.com>
2019-03-18 10:49:09 +0100

    Add files via upload

---
+++ CosaNostra/src/main/java/com/cosanostra/demo/CosaNostraApplication.java
@@ -0,0 +1,13 @@
+package com.cosanostra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosaNostraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosaNostraApplication.class, args);
+	}
+
+}
