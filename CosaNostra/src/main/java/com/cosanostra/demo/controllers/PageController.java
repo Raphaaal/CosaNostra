3ab300db5ff5325adcfa6251965354dcff573138
Victor Fritz <victorfritz.ici@gmail.com>
2019-03-18 11:10:49 +0100

    test commit

---
+++ CosaNostra/src/main/java/com/cosanostra/demo/controllers/PageController.java
@@ -0,0 +1,12 @@
+package com.cosanostra.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PageController {
+	
+	public String index() {
+		return "marc la pute";
+	}
+	
+}
