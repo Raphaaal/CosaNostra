fce57c68f0202818096975409c145d5fe7fd1198
azorra17 <azorra17@b032-27.crio.dauphine.fr>
2019-04-15 10:35:18 +0200

    Added related services for final result query on WikiData

--- CosaNostra/src/main/java/controllers/search/FinalResult.java
+++ CosaNostra/src/main/java/controllers/search/FinalResult.java
@@ -1,18 +1,45 @@
 package controllers.search;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FinalResult {
 		
 	private String name;
 	private String photoUrl;
 	private String pageId;
+	private String desc;
+	private String nationality; // P27
+	private String gender; // P21
+	private String dateOfBirth; // P569
+	private String occupation; // P106
+	private String style; // P136
+	private Map<String, String> relatedPagesIds;
+	private Map<String, String> relatedServices; //MusicBrainz P434, Twitter P2002, Spotify P1902
 	
 	
-	public FinalResult(String name, String photoUrl, String pageId) {
-		super();
+
+	public FinalResult(String name, String photoUrl, String pageId, String desc, String nationality, String gender,
+			String dateOfBirth, String occupation, String style) {
 		this.name = name;
 		this.photoUrl = photoUrl;
 		this.pageId = pageId;
-		
+		this.desc = desc;
+		this.nationality = nationality;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.occupation = occupation;
+		this.style = style;
+		this.relatedPagesIds = new HashMap();
+		this.relatedServices = new HashMap();
+	}
+	
+	@Override
+	public String toString() {
+		return "FinalResult [name=" + name + ", photoUrl=" + photoUrl + ", pageId=" + pageId + ", desc=" + desc
+				+ ", nationality=" + nationality + ", gender=" + gender + ", dateOfBirth=" + dateOfBirth
+				+ ", occupation=" + occupation + ", style=" + style + ", relatedPagesIds=" + relatedPagesIds
+				+ ", relatedServices=" + relatedServices + "]";
 	}
 	public String getName() {
 		return name;
@@ -26,9 +53,54 @@ public class FinalResult {
 	public void setPhotoUrl(String photoUrl) {
 		this.photoUrl = photoUrl;
 	}
+	
 	public String getPageId() {
 		return pageId;
 	}
+	public String getdesc() {
+		return desc;
+	}
+	public void setdesc(String desc) {
+		this.desc = desc;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public Map<String, String> getRelatedPagesIds() {
+		return relatedPagesIds;
+	}
+
+	public Map<String, String> getRelatedServices() {
+		return relatedServices;
+	}
+
 	public void setPageId(String pageId) {
 		this.pageId = pageId;
 	}
--- CosaNostra/src/main/java/controllers/search/ResultQuery.java
+++ CosaNostra/src/main/java/controllers/search/ResultQuery.java
@@ -2,6 +2,7 @@ package controllers.search;
 
 import java.io.IOException;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -15,12 +16,13 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 
 public class ResultQuery {
 	
-	public FinalResult getFinalResult(String pageId) throws IOException, ParseException {
+	public static FinalResult getFinalResult(String pageId) throws IOException, ParseException {
+		
+		FinalResult fRes = new FinalResult(null, null, pageId, null, null, null, null, null, null); 
 		
 		HttpTransport httpTransport = new NetHttpTransport();
 		HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
 		JSONParser parser = new JSONParser();
-
 		GenericUrl baseUrl = new GenericUrl("https://www.wikidata.org/w/api.php?");
 		baseUrl.put("action","wbgetentities");
 		baseUrl.put("ids",pageId);
@@ -30,11 +32,245 @@ public class ResultQuery {
 		HttpRequest finalRequest = requestFactory.buildGetRequest(baseUrl);
 		HttpResponse finalHttpResponse = finalRequest.execute();
 		JSONObject finalResponse = (JSONObject) parser.parse(finalHttpResponse.parseAsString());
+
+		// AJOUT PHOTO DU RESULT
+		Object entities =  finalResponse.get("entities");
+		JSONObject Jentities = (JSONObject) entities;
+		JSONObject Jid = (JSONObject) Jentities.get(fRes.getPageId());
+		Object claims = Jid.get("claims");
+		JSONObject Jclaims = (JSONObject) claims;
+		Object photo = Jclaims.getOrDefault("P18", "wallou photo");
+		if (photo.toString() != "wallou photo") {
+			JSONArray Jphoto = (JSONArray) photo;
+			Object mainsnak = Jphoto.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object photoName = Jvalue.get("value");
+			String finalPhotoName = photoName.toString().replaceAll(" ", "_");
+			fRes.setPhotoUrl("https://commons.wikimedia.org/wiki/Special:FilePath/"+finalPhotoName);
+		}
 		
-		// Se concentrer sur les pages reliées ("pages that link to this page")?
+		// AJOUT NOM DU RESULT
+		Object name = Jclaims.getOrDefault("P1559", "wallou nom");
+		if (name.toString() != "wallou nom") {
+			JSONArray Jname = (JSONArray) name;
+			Object mainsnak = Jname.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object namevalue = Jvalue.get("value");
+			JSONObject Jnamevalue = (JSONObject) namevalue;
+			Object nametext = Jnamevalue.get("text");
+			fRes.setName(nametext.toString());
+		}
+		else {
+			fRes.setName("No name");
+		}
 		
-		return null;
+		//AJOUT DE LA DATE DE NAISSANCE
+		Object dtBirth = Jclaims.getOrDefault("P569", "wallou date de naissance");
+		if (dtBirth.toString() != "wallou date de naissance") {
+			JSONArray JdtBirth = (JSONArray) dtBirth;
+			Object mainsnak = JdtBirth.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object namevalue = Jvalue.get("value");
+			JSONObject Jnamevalue = (JSONObject) namevalue;
+			Object nametext = Jnamevalue.get("time");
+			fRes.setDateOfBirth(nametext.toString());
+		}
+		else {
+			fRes.setDateOfBirth("No date de naissance");
+		}
 		
+		//AJOUT DU SEXE
+		Object gender = Jclaims.getOrDefault("P21", "wallou sexe");
+		if (gender.toString() != "wallou sexe") {
+			JSONArray Jgender = (JSONArray) gender;
+			Object mainsnak = Jgender.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			JSONObject Jvalue2 = (JSONObject) value2;
+			String genderName = getPageName((String) Jvalue2.get("id"));
+			fRes.setGender(genderName.toString());
+		}
+		else {
+			fRes.setGender("No gender");
+		}
+		
+		//AJOUT NATIONALITY
+		Object nationality = Jclaims.getOrDefault("P27", "wallou nationality");
+		if (nationality.toString() != "wallou nationality") {
+			JSONArray Jnationality = (JSONArray) nationality;
+			Object mainsnak = Jnationality.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			JSONObject Jvalue2 = (JSONObject) value2;
+			String nationalityName = getPageName((String) Jvalue2.get("id"));
+			fRes.setNationality(nationalityName.toString());
+		}
+		else {
+			fRes.setNationality("No nationality");
+		}
+		
+		//AJOUT ACCOUPATION
+		Object occupation = Jclaims.getOrDefault("P106", "wallou occupation");
+		if (occupation.toString() != "wallou occupation") {
+			JSONArray Joccupation = (JSONArray) occupation;
+			Object mainsnak = Joccupation.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			JSONObject Jvalue2 = (JSONObject) value2;
+			String nationalityName = getPageName((String) Jvalue2.get("id"));
+			fRes.setOccupation(nationalityName.toString());
+		}
+		else {
+			fRes.setOccupation("No occupation");
+		}
+		
+		//AJOUT STYLE
+		Object style = Jclaims.getOrDefault("P136", "wallou style");
+		if (style.toString() != "wallou style") {
+			JSONArray Jstyle = (JSONArray) style;
+			Object mainsnak = Jstyle.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			JSONObject Jvalue2 = (JSONObject) value2;
+			String nationalityName = getPageName((String) Jvalue2.get("id"));
+			fRes.setStyle(nationalityName.toString());
+		}
+		else {
+			fRes.setStyle("No style");
+		}
+		
+		//AJOUT SERVICES RELIES
+		//MusicBrainz
+		Object musicBrainz = Jclaims.getOrDefault("P434", "wallou musicBrainz");
+		if (musicBrainz.toString() != "wallou musicBrainz") {
+			JSONArray JmusicBrainz = (JSONArray) musicBrainz;
+			Object mainsnak = JmusicBrainz.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			fRes.getRelatedServices().put("MusicBrainz", "https://musicbrainz.org/artist/"+value2.toString());
+		}
+		//Twitter
+		Object twitter = Jclaims.getOrDefault("P2002", "wallou twitter");
+		if (twitter.toString() != "wallou twitter") {
+			JSONArray Jtwitter = (JSONArray) twitter;
+			Object mainsnak = Jtwitter.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			fRes.getRelatedServices().put("Twitter", "https://twitter.com/"+value2.toString());
+		}
+		
+		//Spotify
+		Object spotify = Jclaims.getOrDefault("P1902", "wallou spotify");
+		if (spotify.toString() != "wallou spotify") {
+			JSONArray Jspotify = (JSONArray) spotify;
+			Object mainsnak = Jspotify.get(0);
+			JSONObject Jmainsnak = (JSONObject) mainsnak;
+			Object datavalue = Jmainsnak.get("mainsnak");
+			JSONObject Jdatavalue = (JSONObject) datavalue;
+			Object value = Jdatavalue.get("datavalue");
+			JSONObject Jvalue = (JSONObject) value;
+			Object value2 = Jvalue.get("value");
+			fRes.getRelatedServices().put("Spotify", "https://open.spotify.com/artist/"+value2.toString());
+		}
+				
+		return fRes;
+		
+	}
+	
+	
+	public static String getPageName(String pageId) throws IOException, ParseException {
+		HttpTransport httpTransport = new NetHttpTransport();
+		HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+		JSONParser parser = new JSONParser();
+		GenericUrl baseUrl = new GenericUrl("https://www.wikidata.org/w/api.php?");
+		baseUrl.put("action","wbgetentities");
+		baseUrl.put("ids",pageId);
+		baseUrl.put("props","labels");
+		baseUrl.put("languages","fr");
+		baseUrl.put("format", "json");
+		HttpRequest finalRequest = requestFactory.buildGetRequest(baseUrl);
+		HttpResponse finalHttpResponse = finalRequest.execute();
+		JSONObject finalResponse = (JSONObject) parser.parse(finalHttpResponse.parseAsString());
+		
+		Object entities = finalResponse.get("entities");
+		JSONObject Jentities = (JSONObject) entities;
+		Object page = Jentities.get(pageId);
+		JSONObject Jpage = (JSONObject) page;
+		Object labels = Jpage.get("labels");
+		JSONObject Jlabels = (JSONObject) labels;
+		Object lang = Jlabels.get("fr");
+		JSONObject Jlang = (JSONObject) lang;
+		Object value = Jlang.get("value");
+		
+		return value.toString();
+	}
+	
+	/*
+	public static String getSummary(String title) throws IOException, ParseException {
+		HttpTransport httpTransport = new NetHttpTransport();
+		HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+		JSONParser parser = new JSONParser();
+		GenericUrl baseUrl = new GenericUrl("https://www.wikipedia.org/w/api.php?");
+		baseUrl.put("action","query");
+		baseUrl.put("titles",title);
+		baseUrl.put("prop","extracts");
+		baseUrl.put("format", "json");
+		HttpRequest finalRequest = requestFactory.buildGetRequest(baseUrl);
+		HttpResponse finalHttpResponse = finalRequest.execute();
+		JSONObject finalResponse = (JSONObject) parser.parse(finalHttpResponse.parseAsString());
+		
+		Object query = finalResponse.get("query");
+		JSONObject Jquery = (JSONObject) entities;
+		Object page = Jentities.get(pageId);
+		JSONObject Jpage = (JSONObject) page;
+		Object labels = Jpage.get("labels");
+		JSONObject Jlabels = (JSONObject) labels;
+		Object lang = Jlabels.get("fr");
+		JSONObject Jlang = (JSONObject) lang;
+		Object value = Jlang.get("value");
+		
+		return value.toString();
+	}
+	*/
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		System.out.println(getFinalResult("Q642477"));
 	}
 
 }
