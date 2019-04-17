576bef7f8a6f057547d5ee83a8167222b84361b0
StanislasTran <transtanislas@gmail.com>
2019-04-17 14:08:59 +0200

    remove uselss com

--- CosaNostra/src/main/java/connect/MySQLConnUtils.java
+++ CosaNostra/src/main/java/connect/MySQLConnUtils.java
@@ -19,10 +19,7 @@ public class MySQLConnUtils {
 
 	public static Connection getMySQLConnection(String hostName, String dbName, String userName, String password)
 			throws SQLException, ClassNotFoundException {
-		// Declare the class Driver for MySQL DB
-		// This is necessary with Java 5 (or older)
-		// Java6 (or newer) automatically find the appropriate driver.
-		// If you use Java> 5, then this line is not needed.
+		
 		Class.forName("com.mysql.jdbc.Driver");
 
 		String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
