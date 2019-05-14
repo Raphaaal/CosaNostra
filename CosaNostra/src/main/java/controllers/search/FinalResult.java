45edc974c46d68f2268ebe52e184668dfbb16000
azorra17 <azorra17@b032-16.crio.dauphine.fr>
2019-05-14 09:28:31 +0200

    Added Javadoc and Comments

--- CosaNostra/src/main/java/controllers/search/FinalResult.java
+++ CosaNostra/src/main/java/controllers/search/FinalResult.java
@@ -5,25 +5,29 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * This class is used to gather detailed information about the final search result the user chose 
+ * (after being presented with multiple potential matches to his/her query).
+ *
+ */
 public class FinalResult {
 		
 	private String name;
 	private String photoUrl;
 	private String pageId;
 	private String desc;
-	private String nationality; // P27
-	private String gender; // P21
-	private String dateOfBirth; // P569
-	private String occupation; // P106
-	private String style; // P136
-	private String instanceOf;//P31
+	private String nationality; 
+	private String gender; 
+	private String dateOfBirth; 
+	private String occupation; 
+	private String style; 
+	private String instanceOf;
 	private Map<String, String> identity;
 	private Map<String, String> relatedPagesIds;
-	private Map<String, String> relatedServices; //MusicBrainz P434, Twitter P2002, Spotify P1902
+	private Map<String, String> relatedServices; 
 	private List<String> backlinks;
 
-	public FinalResult(String name, String photoUrl, String pageId, String desc, String nationality, String gender,
-			String dateOfBirth, String occupation, String style,String instanceOf) {
+	public FinalResult(String name, String photoUrl, String pageId, String desc, String nationality, String gender, String dateOfBirth, String occupation, String style,String instanceOf) {
 		this.name = name;
 		this.photoUrl = photoUrl;
 		this.pageId = pageId;
@@ -51,7 +55,6 @@ public class FinalResult {
 		return backlinks;
 	}
 	
-	
 	public void setBacklinks(List<String> backlinks) {
 		this.backlinks = backlinks;
 	}
@@ -60,13 +63,9 @@ public class FinalResult {
 		return instanceOf;
 	}
 
-
-
 	public void setInstanceOf(String instanceOf) {
 		this.instanceOf = instanceOf;
 	}
-
-
 
 	public Map<String, String> getIdentity() {
 		if(dateOfBirth != null) {
@@ -89,12 +88,15 @@ public class FinalResult {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getPhotoUrl() {
 		return photoUrl;
 	}
+	
 	public void setPhotoUrl(String photoUrl) {
 		this.photoUrl = photoUrl;
 	}
@@ -102,42 +104,53 @@ public class FinalResult {
 	public String getPageId() {
 		return pageId;
 	}
+	
 	public String getDesc() {
 		return desc;
 	}
+	
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
 	public String getNationality() {
 		return nationality;
 	}
+	
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
+	
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	
 	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
+	
 	public void setDateOfBirth(String dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+	
 	public String getOccupation() {
 		return occupation;
 	}
+	
 	public void setOccupation(String occupation) {
 		this.occupation = occupation;
 	}
+	
 	public String getStyle() {
 		return style;
 	}
+	
 	public void setStyle(String style) {
 		this.style = style;
 	}
+	
 	public Map<String, String> getRelatedPagesIds() {
 		return relatedPagesIds;
 	}
@@ -149,6 +162,4 @@ public class FinalResult {
 	public void setPageId(String pageId) {
 		this.pageId = pageId;
 	}
-
-
 }
